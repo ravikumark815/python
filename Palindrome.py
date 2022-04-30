e84e1b3d0afcf5fedbc1550117bd7eaf8f7afad0
Ravi Kumar Reddy Koothati <ravikumark815@gmail.com>
2022-04-30 11:44:42 +0530

    Palindrome

---
+++ Palindrome.py
@@ -0,0 +1,34 @@
+"""
+Given an integer x, return true if x is palindrome integer.
+
+An integer is a palindrome when it reads the same backward as forward.
+
+For example, 121 is a palindrome while 123 is not.
+
+Example 1:
+
+Input: x = 121
+Output: true
+Explanation: 121 reads as 121 from left to right and from right to left.
+Example 2:
+
+Input: x = -121
+Output: false
+Explanation: From left to right, it reads -121. From right to left, it becomes 121-. Therefore it is not a palindrome.
+Example 3:
+
+Input: x = 10
+Output: false
+Explanation: Reads 01 from right to left. Therefore it is not a palindrome.
+
+"""
+
+def isPalindrome(num):
+    return (str(num) == str(num)[::-1])
+
+def main():
+    x = 121
+    print(isPalindrome(x))
+
+if __name__=="__main__":
+    main()
