94f3b5ceaed1e3504c20e0b3c3ed35253d1bbfe9
Ravi Kumar Reddy K <ravikumark815@gmail.com>
2023-02-09 10:35:48 +0530

    TwoSum

--- TwoSum.py
+++ TwoSum.py
@@ -19,20 +19,17 @@ Output: [0,1]
 """
 
 def twoSum(nums, target):
-        result = []
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if i==j:
-                    continue
-                elif ((nums[i] + nums[j]) == target):
-                    result.append(i)
-                    result.append(j)
-                    return result
+    for i in range(len(nums)):
+        for j in range(i+1, len(nums)):
+            if ((nums[i] + nums[j]) == target):
+                return [i,j]
 
 def main():
+    testcases = [[[2,7,11,15], 9], [[3,2,4], 6], [[3,3], 6]]
     nums = [2,7,11,15]
     target = 9
-    print(twoSum(nums, target))
+    for tc in testcases:
+        print("TC:",tc[0], "Target:",tc[1], "Result:",twoSum(tc[0], tc[1]))
 
 if __name__=="__main__":
     main()
